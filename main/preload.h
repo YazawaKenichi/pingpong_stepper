ab479602349d35eda45baebc0100874308b2cb11
YazawaKenichi <tonfarken83825@gmail.com>
2025-12-12 17:46:17 +0900

    Revert "一つのファイルに統合して serial 出しやすくした"
    This reverts commit b940518f48cc50c293785b79f9d3296701a210f4.

---
+++ main/preload.h
@@ -0,0 +1,16 @@
+#ifndef __PRELOAD_H__
+#define __PRELOAD_H__
+
+#define DIR_PIN 8
+#define STEP_PIN 9
+#define ENABLE_PIN 7
+#define SLEEP_PIN 13
+#define MS1_PIN 10
+#define MS2_PIN 11
+#define MS3_PIN 12
+
+#define LEFT_LIMIT_PIN 2
+#define RIGHT_LIMIT_PIN 3
+
+#endif
+
