f7872f1e96a8a8d4b2a65193625f54b631fe3027
YazawaKenichi <tonfarken83825@gmail.com>
2025-12-12 15:34:14 +0900

    速度遅くしたら初動で脱調しなくなった

--- main/main.ino
+++ main/main.ino
@@ -45,9 +45,9 @@ void loop() {
   // Spin motor quickly
   for (int x = 0; x < stepsPerRevolution; x++) {
     digitalWrite(stepPin, HIGH);
-    delayMicroseconds(1000);
+    delayMicroseconds(2000);
     digitalWrite(stepPin, LOW);
-    delayMicroseconds(1000);
+    delayMicroseconds(2000);
   }
   delay(1000);  // Wait a second
 }
